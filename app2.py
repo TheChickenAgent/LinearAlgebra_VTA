9f7b4532d1c861c3f711f8e8c97f84488290439b
TheChickenAgent <martijn.elands@gmail.com>
2025-05-07 21:18:06 +0200

    Drafting

---
+++ app2.py
@@ -0,0 +1,81 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+def intro():
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
+
+    st.markdown(
+        """
+        Streamlit is an open-source app framework built specifically for
+        Machine Learning and Data Science projects.
+
+        **👈 Select a demo from the dropdown on the left** to see some examples
+        of what Streamlit can do!
+
+        ### Want to learn more?
+
+        - Check out [streamlit.io](https://streamlit.io)
+        - Jump into our [documentation](https://docs.streamlit.io)
+        - Ask a question in our [community
+          forums](https://discuss.streamlit.io)
+
+        ### See more complex demos
+
+        - Use a neural net to [analyze the Udacity Self-driving Car Image
+          Dataset](https://github.com/streamlit/demo-self-driving)
+        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    """
+    )
+
+
+def plotting_demo():
+    # Load environment variables from a .env file
+    load_dotenv()
+    OPENAI_API = os.getenv('OPENAI_API_KEY')
+
+    ##### Streamlit app
+
+    st.title("ChatGPT-like clone")
+
+    client = OpenAI(api_key=OPENAI_API)
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("What is up?"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+page_names_to_funcs = {
+    "—": intro,
+    "Linear Algebra chat Demo": plotting_demo,
+    #"Mapping Demo": mapping_demo,
+    #"DataFrame Demo": data_frame_demo
+}
+
+demo_name = st.sidebar.selectbox("Choose a demo", page_names_to_funcs.keys())
+page_names_to_funcs[demo_name]()
